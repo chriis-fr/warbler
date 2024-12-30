25696feadf64b9c601c676f1ef180d56f31e221d
chriis-fr <caspianodhis@gmail.com>
2024-12-30 13:41:16 +0300

    3

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -9,11 +9,11 @@ const Book = () => {
     <>
       <div className="flex flex-col   w-full  h-screen bg-black/[0.96]  bg-grid-white/[0.02]   ">
         <Navbar
-          scrollToWebsiteDesign={() => {}}
-          scrollToGraphicDesign={() => {}}
-          scrollToShopifyStores={() => {}}
-          scrollToBrands={() => {}}
-          scrollToServices={() => {}}
+          // scrollToWebsiteDesign={() => {}}
+          // scrollToGraphicDesign={() => {}}
+          // scrollToShopifyStores={() => {}}
+          // scrollToBrands={() => {}}
+          // scrollToServices={() => {}}
         />
         <div className="text-4xl pb-5 md:text-6xl text-center 
         bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
